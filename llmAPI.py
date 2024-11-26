37265091c2bf86e124214641107bacabf4352479
Quentin <quentin.muench@web.de>
2024-11-26 17:28:16 +0100

    added backup dataset, started first synthesis

--- llmAPI.py
+++ llmAPI.py
@@ -39,24 +39,37 @@ def synthesis_from_papers(filepath="data/synthesized_papers.xlsx"):
         base_url = base_url
     )
 
-    SYSTEM_PROMPT = "You are a scholarly assistant tasked with answering research questions by synthesizing information from the abstracts and titles of scholarly articles."
+    SYSTEM_PROMPT = "You are a scholarly assistant tasked with answering scientific questions by synthesizing information from the abstracts and titles of scholarly articles."
 
     for index, row in df.iterrows():
         print("###############################################")
         print(f"Processing row {index}")
 
         question = row["research_question"]
-        paper1_content = f"Title: {row['paper_1_title']}. Abstract: {row['paper_1_abstract']}"
-        paper2_content = f"Title: {row['paper_2_title']}. Abstract: {row['paper_2_abstract']}"
-        paper3_content = f"Title: {row['paper_3_title']}. Abstract: {row['paper_3_abstract']}"
-        paper4_content = f"Title: {row['paper_4_title']}. Abstract: {row['paper_4_abstract']}"
-        paper5_content = f"Title: {row['paper_5_title']}. Abstract: {row['paper_5_abstract']}"
+        papers_content = []
+        for val in row[1:]:
+            if pd.isnull(val):
+                break  
+            
+            papers_content.append(val)
+        
+        paper_string = ""
+        for i, content in enumerate(papers_content):
+            if i % 2 == 0:
+                paper_string += f"Paper {(i//2) + 1}:\nTitle: {content}\n"
+            else:
+                paper_string += f"Abstract: {content}\n\n"
+            
 
-        user_prompt = f"Generate a synthesis from the provided papers as content on the research question '{question}' into a concise single paragraph of no more than 200 words. Follow these instructions: \n - Only the titles and abstracts will be provided from up to five scientific papers which are to be used as the content for the synthesis. \n - The objective of this synthesis is to provide a paperwise analysis. Therefore, summarize each paper's contributions individually to the given research question above, noting significant findings or methodologies, or other such salient contribution facets. \n - Support each claim with citations, formatted as (1) or (3, 5) to refer to the respective papers' content, where the numbers correspond to the list of provided papers. \n - Ensure the output is formatted as a single cohesive paragraph without section headings, titles, abstracts, or any paper-like structure. The focus should be on integrating and synthesizing the content into a unified narrative. \n - Focus on essential information, maintaining clarity and precision. \n - Do not include additional information or exceed the specified word count of 200 words and the single paragraph synthesis output requirement.\n\n Papers \n 1. '{paper1_content}' \n\n 2. '{paper2_content}' \n\n 3. '{paper3_content}' \n\n 4. '{paper4_content}' \n\n 5. '{paper5_content}'"
+        # user_prompt_llm4syn = f"Generate a synthesis from the provided papers as content on the research question '{question}' into a concise single paragraph of no more than 200 words. Follow these instructions: \n - Only the titles and abstracts will be provided from up to five scientific papers which are to be used as the content for the synthesis. \n - The objective of this synthesis is to provide a paperwise analysis. Therefore, summarize each paper's contributions individually to the given research question above, noting significant findings or methodologies, or other such salient contribution facets. \n - Support each claim with citations, formatted as (1) or (3, 5) to refer to the respective papers' content, where the numbers correspond to the list of provided papers. \n - Ensure the output is formatted as a single cohesive paragraph without section headings, titles, abstracts, or any paper-like structure. The focus should be on integrating and synthesizing the content into a unified narrative. \n - Focus on essential information, maintaining clarity and precision. \n - Do not include additional information or exceed the specified word count of 200 words and the single paragraph synthesis output requirement.\n\n {paper_string}"
+
+        user_prompt_bioasq = f"Generate an answer to the scientific question '{question}' from the provided papers into a concise single paragraph of 1 to 5 sentences. Follow these instructions: \n - Only the titles and abstracts will be provided from up to forty scientific papers which are to be used as the content for the answer sentence or sentences. \n - Comprehend the question. \n - The objective of your task is to answer the given question based on the evidence content from the respective papers. Therefore read all the papers’ title and abstract and determine the portions of text where the answer is present. \n - Synthesize the answer based on evidence from the papers, avoiding repetition if the same information appears in multiple sources. \n - If you find the answer in multiple papers, paraphrase the answer once, cite all the sources from which the answer was obtained. In other words, support each answer sentence with citations, formatted as (1) or (3, 5) to refer to the respective papers' content, where the numbers correspond to the list of provided papers. \n - Ensure the output is formatted as a single cohesive paragraph without section headings, titles, abstracts, or any paper-like structure. The focus should be on integrating and synthesizing the content into a unified narrative that constitutes the answer. \n - Focus on essential information that directly answers the question, maintaining clarity and precision. \n - Do not include additional information or exceed the specified number of sentences of between 1 to 5 sentences in the expected answer output requirement.\n\n {paper_string}"
+
+        user_prompt = user_prompt_bioasq
 
         for model in MODELS:
-            # if not pd.isnull(row[f"{model}_synthesis"]):
-            #    continue
+            if not pd.isnull(row[f"{model}_synthesis"]):
+                continue
 
             print(f"Using model: {model}")
 
@@ -158,9 +171,8 @@ def test():
 if __name__ == "__main__":
     # prepare_data()
     
-    files = ["data/12B1_golden.xlsx", "data/12B2_golden.xlsx", "data/12B3_golden.xlsx", "data/12B4_golden.xlsx"]
-    for file in files:
-        synthesis_from_papers(file)
+    filepath = "data/BioASQ_dataset.xlsx"
+    synthesis_from_papers(filepath)
     
     # check_df()
     # check_models()
