a01dd44de195c9b6e74703e66eba935e1e80e5f0
Quentin <quentin.muench@web.de>
2025-02-02 20:48:10 +0100

    update quantitative evaluation

--- quantitive_eval_data_extraction.py
+++ quantitive_eval_data_extraction.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 METRICS = ["bertscoreF1", "moverscore", "wmd_specter", "wmd_scibert", "bleu", "rouge1F1", "rouge2F1", "rouge4F1", "rougeLF1", "nist", "meteor", "wer"]
-
+CONVERSION = {"meta-llama-3.1-70b-instruct": "Llama 70B", "meta-llama-3.1-8b-instruct": "Llama 8B", "mistral-large-instruct": "Mistral", "qwen2.5-72b-instruct": "Qwen" }
 
 def generate_table(file_path, start_i):
     df = pd.read_excel(file_path)
@@ -18,8 +18,8 @@ def generate_table(file_path, start_i):
         if metric == "wmd":
             metric += "_" + split_col[1]
             i += 1
-        reference = split_col[i]
-        candidate = split_col[i+1]
+        reference = CONVERSION[split_col[i]]
+        candidate = CONVERSION[split_col[i+1]]
 
         mean_dict[metric].append((reference, candidate, df[col].mean()))
 
@@ -36,15 +36,15 @@ def generate_table(file_path, start_i):
         plt.xlabel("Candidate", fontsize=14)
         plt.ylabel("Reference", fontsize=14)
         plt.tight_layout()
-        plt.savefig(f"data/plots/clean/{file_path.split("/")[-1].split("_")[0]}_{metric}.png")
+        plt.savefig(f"data/temp/clean_plots/{file_path.split("/")[-1].split("_")[0]}_{metric}.png")
         plt.close()
         # plt.show()
         
 
 
 def main():
-    generate_table("data/BioASQ_dataset_eval_clean.xlsx", 85)
-    generate_table("data/llm4syn_dataset_eval_clean.xlsx", 23)
+    generate_table("data/quantitative_eval_scores/BioASQ_dataset_quantitative.xlsx", 85)
+    generate_table("data/quantitative_eval_scores/ORKGSyn_dataset_quantitative.xlsx", 23)
 
 
 if __name__ == "__main__":
